faa554fd43bd5e3f75bc1c02c7d0221ddeea4f4d
Shaldon <user@user>
2022-01-23 11:23:37 +0300

    updated sass to dart-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,43 @@
 const { src, dest, watch } = require('gulp');
-const rename        = require('gulp-rename');
-const nunjucks      = require('gulp-nunjucks');
-const prettifyHtml  = require('gulp-pretty-html');
-const del           = require('del');
-const sass          = require('gulp-sass');
-sass.compiler       = require('node-sass');
+const rename = require('gulp-rename');
+const nunjucks = require('gulp-nunjucks');
+const prettifyHtml = require('gulp-pretty-html');
+const del = require('del');
+const liveServer = require('live-server');
+const sass = require('gulp-sass')(require('sass'));
 const removeEmptyLines = require('gulp-remove-empty-lines');
 
 
+// config
+const TEMPLATE_PATHS = [
+    './src/templates/**/*.njk',
+    '!./src/templates/parts/**/*',
+    '!./src/templates/macro/**/*'
+];
 
-const template = () => (
-    src([   
-        './src/templates/**/*.njk',
-        '!./src/templates/parts/**/*',
-        '!./src/templates/macro/**/*'
-    ])
+const STYLE_PATHS = ['./src/styles/**/*.scss'];
+
+
+// tasks
+const template = () => {
+
+    const errorHandler = function (err) {
+        console.error(err.toString());
+        this.emit('end');
+    }
+
+    src(TEMPLATE_PATHS)
         .pipe(nunjucks.compile())
+        .on('error', errorHandler)
         .pipe(rename({ extname: '.html' }))
         .pipe(prettifyHtml())
         .pipe(removeEmptyLines())
         .pipe(dest('dist'))
-);
+};
 
 
 const styles = () => (
-    src('./src/styles/**/*.scss')
+    src(STYLE_PATHS)
         .pipe(sass().on('error', sass.logError))
         .pipe(dest('./dist/styles'))
 );
@@ -39,9 +52,21 @@ const scripts = () => (
 const clean = () => del(['dist/*'])
 
 
-const buildOnChange = () =>  (
-    watch('src/**/*').on('all',(path,file) => {
-        //console.log(`Operation: ${path}; File: ${file}`)
+const allTasks = () => (template(), styles(), scripts())
+
+
+const devServer = () => {
+
+    liveServer.start({
+        host: '0.0.0.0',
+        port: 5555,
+        wait: 500,
+        root: 'dist'
+    });
+
+    allTasks();
+    
+    watch('src/**/*').on('all', (path, file) => {
         console.log('Rebuild running...');
 
         template();
@@ -50,13 +75,13 @@ const buildOnChange = () =>  (
 
         console.log('Rebuild completed.');
     })
-);
+}
 
-exports.default = () => (template(), styles(), scripts())
+exports.default = allTasks;
 
 
-exports.buildOnChange = buildOnChange;
+exports.devServer = devServer;
 exports.build = template;
 exports.sass  = styles;
-exports.js    = scripts;
+exports.js = scripts;
 exports.clean = clean;
